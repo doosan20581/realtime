af003a6725b396c6016f324cc8cbbd8ffa0a04a5
최원석 <doosan20581@gmail.com>
2026-03-12 12:11:22 +0900

    chore: 테스트용 디펜던시 추가 및 디펜던시 목록 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	java
-	id("org.springframework.boot") version "4.0.1"
+	//id("org.springframework.boot") version "4.0.1"
+	id("org.springframework.boot") version "3.3.6"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -28,18 +29,20 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	implementation("com.mysql:mysql-connector-j")
 	
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	
+	testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+	
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-websocket-test")
 	
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	
 	//h2 설정
-	runtimeOnly("com.h2database:h2")
+	//runtimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {
