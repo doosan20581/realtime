c4f7c886b3544a23eccd5e560faefc79c2d8576f
최원석 <doosan20581@gmail.com>
2026-03-27 18:34:48 +0900

    feat: 회원가입, 로그인시 refresh token 발급 추가

--- src/main/java/com/vibe/realtime/auth/service/AuthService.java
+++ src/main/java/com/vibe/realtime/auth/service/AuthService.java
@@ -1,7 +1,10 @@
 package com.vibe.realtime.auth.service;
 
+import java.time.Duration;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -28,31 +31,63 @@ public class AuthService {
 	private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final JwtProvider jwtProvider;
-
+    private final RedisTemplate<String, String> redisTemplate;
+    
+    /**
+     * 신규 회원가입
+     * 
+     * @param request
+     * @return
+     */
 	public AuthResponse signup(SignupRequest request) {
-
+		
+		// 1. 신규 회원 레코드 생성
 	    User user = userService.createUser(
 	        request.getEmail(),
 	        request.getPassword(),
 	        request.getName()
 	    );
 	    
-	    // roles 포함 JWT 생성
+	    // 2-1. access_jti 생성
+	    String access_jti = UUID.randomUUID().toString(); // 토큰 고유 ID
+	    
+	    // 2-2. ACCESS TOKEN 생성
 	    String accessToken = jwtProvider.createAccessToken(
 	        user.getId(),
 	        user.getRoles().stream()
 	            .map(role -> new SimpleGrantedAuthority(role.getName()))
-	            .collect(Collectors.toList())
+	            .collect(Collectors.toList()),
+	        access_jti
 	    );
+	    
+	    // 3-1. refresh_jti 생성
+	    String refresh_jti = UUID.randomUUID().toString(); // 토큰 고유 ID
+	    
+	    // 3-2. REFRESH TOKEN 생성
+        // 현재 aws 기본 환경 세팅 완료 (ec2, elasticache redis oss)
+        // 로컬 개발시 로컬 pc 에서 ssh 터널링으로 6379 포트 redis 통신 예정, 로컬 작업지에 ec2 ssh 접속용 개인키 설치 확인
+        String refreshToken = jwtProvider.createRefreshToken(user.getId(), refresh_jti);
+        
+        // 3-3. Redis 저장
+        String redisKey = "RT:" + user.getId();
 
+        redisTemplate.opsForValue().set(
+            redisKey,
+            refresh_jti,
+            Duration.ofMillis(jwtProvider.getRefreshTokenExpirySeconds() * 1000)
+        );
+        
+        // 4. TokenResponse 생성 - 백엔드용 context
 	    TokenResponse tokenResponse = TokenResponse.builder()
 	            .accessToken(accessToken)
-	            .refreshToken(null)
+	            .refreshToken(refreshToken)
 	            .expiresIn(jwtProvider.getAccessTokenExpirySeconds())
 	            .build();
-
+	    
+	    // 5. UserResponse 생성 - 프론트엔드용 context
 	    UserResponse userResponse = UserResponse.from(user);
-
+	    
+	    // 6. AuthResponse 반환
 	    return AuthResponse.builder()
 	            .token(tokenResponse)
 	            .user(userResponse)
@@ -60,6 +95,12 @@ public class AuthService {
 
 	}
 	
+	/**
+	 * 회원 로그인
+	 * 
+	 * @param request
+	 * @return
+	 */
 	public AuthResponse login(LoginRequest request) {
 		
 		String email = request.getEmail();
@@ -74,26 +115,42 @@ public class AuthService {
 
         // 2️. CustomUserDetails 추출
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        
+        // 3-1. access_jti 생성
+	    String access_jti = UUID.randomUUID().toString(); // 토큰 고유 ID
 
-        // 3️-1. ACCESS TOKEN 생성
+        // 3️-2. ACCESS TOKEN 생성
         String accessToken = jwtProvider.createAccessToken(
             userDetails.getUserId(),
-            userDetails.getAuthorities()
+            userDetails.getAuthorities(),
+            access_jti
         );
         
-        // 3-2. (추가) REFRESH TOKEN 생성
-        // 작업 필요
+        // 4-1. refresh_jti 생성
+	    String refresh_jti = UUID.randomUUID().toString(); // 토큰 고유 ID
+        
+        // 4-2. REFRESH TOKEN 생성
         // 현재 aws 기본 환경 세팅 완료 (ec2, elasticache redis oss)
         // 로컬 개발시 로컬 pc 에서 ssh 터널링으로 6379 포트 redis 통신 예정, 로컬 작업지에 ec2 ssh 접속용 개인키 설치 확인
+        String refreshToken = jwtProvider.createRefreshToken(userDetails.getUserId(), refresh_jti);
+        
+        // 4-3. Redis 저장
+        String redisKey = "RT:" + userDetails.getUserId();
 
-        // 3-3 JWT 생성
+        redisTemplate.opsForValue().set(
+            redisKey,
+            refresh_jti,
+            Duration.ofMillis(jwtProvider.getRefreshTokenExpirySeconds() * 1000)
+        );
+
+        // 5. TokenResponse 생성 - 백엔드용 context
         TokenResponse tokenResponse = TokenResponse.builder()
             .accessToken(accessToken)
-            .refreshToken(null) // (추가) 현재는 null 이지만, 이제 refresh token 작성하는 부분 작업하여서 처리 필요
+            .refreshToken(refreshToken)
             .expiresIn(jwtProvider.getAccessTokenExpirySeconds())
             .build();
 
-        // 4️. UserResponse 생성
+        // 6. UserResponse 생성 - 프론트엔드용 context
         UserResponse userResponse = UserResponse.builder()
             .id(userDetails.getUserId())
             .name(userDetails.getName())
@@ -104,7 +161,7 @@ public class AuthService {
                 .collect(Collectors.toList()))
             .build();
 
-        // 5️. AuthResponse 반환
+        // 7. AuthResponse 반환
         return AuthResponse.builder()
             .token(tokenResponse)
             .user(userResponse)
--- src/main/java/com/vibe/realtime/common/config/security/JwtProvider.java
+++ src/main/java/com/vibe/realtime/common/config/security/JwtProvider.java
@@ -11,8 +11,6 @@ import java.util.stream.Collectors;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
-import com.vibe.realtime.user.entity.Role;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -24,6 +22,19 @@ import io.jsonwebtoken.security.Keys;
  * - In-memory 랜덤 키 사용: 서버 재시작 시 키가 변경됨
  *   → 기존 토큰은 검증 불가
  * - 실제 서비스에서는 환경변수/설정 파일에 고정 시크릿 키(jwt.secret) 사용 필요
+ * 
+ * Access Token vs Refresh Token 차이
+ * 
+ * 용도 - 인증/인가	vs 재발급
+ * 포함 정보 - userId + roles vs userId
+ * 만료시간 - 짧음 (1시간) vs 김 (1일)
+ * 저장 위치 - 클라이언트 vs	Redis (AWS ElastiCache Redis OSS)
+ * 
+ * jti 설정
+ * access token - logout 시 남은 만료시간 만큼의 ttl로 blacklist 데이터를 redis 에 저장하여, 로그아웃 처리된 access token 재사용 방지 처리
+ * refresh token - 사용자의 멀티 디바이스 환경에서 각각의 세션을 관리 및 refresh 토큰 탈취 방지 처리
+ * 
+ * key (시크릿 키) - 해당 payload의 무결성 확인
  */
 @Component
 public class JwtProvider {
@@ -32,10 +43,11 @@ public class JwtProvider {
 	// application.yaml 또는 환경 변수로 고정된 시크릿 키 사용방식을, 간소화 처리
     private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256); // 시크릿 키 자동 생성
     private final long ACCESS_TOKEN_VALIDITY = 1000L * 60 * 60; // 1시간 - Access Token
+    private final long REFRESH_TOKEN_VALIDITY = 1000L * 60 * 60 * 24 * 1; // 1일 - Refresh Token
 
     // Access Token 생성 = JWT 발급
     // 핵심: JWT 생성 시 DB 엔티티(Role)에 직접 접근하지 않음
-    public String createAccessToken(Integer userId, Collection<? extends GrantedAuthority> authorities) {
+    public String createAccessToken(Integer userId, Collection<? extends GrantedAuthority> authorities, String jti) {
     	Date now = new Date();
         Date expiryDate = new Date(now.getTime() + ACCESS_TOKEN_VALIDITY);
     	
@@ -45,14 +57,32 @@ public class JwtProvider {
 
     	return Jwts.builder()
                 .setSubject("at") // 토큰 타입
-                .claim("userId", userId)    // 사용자 식별값
-                .claim("roles", roleNames)  // 사용자 권한
+                .claim("userId", userId) // 사용자 식별값
+                .claim("roles", roleNames) // 사용자 권한
+                .setId(jti)
                 .setIssuedAt(now)
                 .setExpiration(expiryDate)
                 .signWith(key)
                 .compact();
     }
-
+    
+    // ----------------------------
+    // Refresh Token 생성
+    // 권한 정보 넣지 않고 가볍게 유지
+    // ----------------------------
+    public String createRefreshToken(Integer userId, String jti) {
+    	Date now = new Date();
+    	Date expiryDate = new Date(now.getTime() + REFRESH_TOKEN_VALIDITY);
+    	
+    	return Jwts.builder()
+    			.setSubject("rt") // 토큰 타입
+    			.claim("userId", userId) // 사용자 식별값
+    			.setId(jti)
+    			.setIssuedAt(now)
+    			.setExpiration(expiryDate)
+    			.signWith(key)
+    			.compact();
+    }
 
     // ----------------------------
     // 토큰 유효성 검사 = 토큰 유효성 확인 (만료, 변조 등)
@@ -116,5 +146,21 @@ public class JwtProvider {
     public long getAccessTokenExpirySeconds() {
         return ACCESS_TOKEN_VALIDITY / 1000L; // 밀리초 → 초 단위
     }
-
+    
+    // ----------------------------
+    // REFRESH 토큰 만료시간 리턴
+    // ----------------------------
+    public long getRefreshTokenExpirySeconds() {
+        return REFRESH_TOKEN_VALIDITY / 1000L;
+    }
+    
+    // jti 추출
+    public String getJtiFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getId(); // jti 반환
+    }
 }
